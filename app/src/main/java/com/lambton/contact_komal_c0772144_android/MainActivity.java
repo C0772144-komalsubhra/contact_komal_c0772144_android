c85d3eba49dca0c0897febdb2f30b8236333a1de
komalcuty40@gmail.com <komalsubhra6768@gmail.com>
2020-07-16 17:25:40 -0400

    initial

--- app/src/main/java/com/lambton/contact_komal_c0772144_android/MainActivity.java
+++ app/src/main/java/com/lambton/contact_komal_c0772144_android/MainActivity.java
@@ -3,8 +3,9 @@ package com.lambton.contact_komal_c0772144_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
