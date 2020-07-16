7128be17b32931ab2f63fb8f59c8a7a36510a852
komalcuty40@gmail.com <komalsubhra6768@gmail.com>
2020-07-16 17:23:23 -0400

    Initial commit

---
+++ app/src/main/java/com/lambton/contact_komal_c0772144_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lambton.contact_komal_c0772144_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
